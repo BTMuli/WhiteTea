9a4a3dda408e66678ad754440fa279f6ec0f10d6
BTMuli <BT-Muli@outlook.com>
2023-10-15 20:13:56 +0800

    ✏️ 完善类型

--- src/types/repo-class.d.ts
+++ src/types/repo-class.d.ts
@@ -12,6 +12,30 @@
  */
 declare namespace MuBot.RepoClass {
   /**
+   * @description 仓库类 - label 枚举
+   * @since 1.0.0
+   * @todo 目前只写了用到的，后续有需要再补充
+   * @enum {number}
+   * @memberof MuBot.RepoClass
+   * @property {number} WIP WIP
+   * @property {number} Done Done
+   * @property {number} Undo Undo
+   * @return {number} label 枚举
+   */
+  enum Label {
+    WIP,
+    Done,
+    Undo,
+  }
+
+  /**
+   * @description 仓库类 - label 枚举的 key
+   * @since 1.0.0
+   * @return LabelKey
+   */
+  type LabelKey = keyof typeof Label;
+
+  /**
    * @description 仓库类 - 配置
    * @since 1.0.0
    * @interface Config
@@ -20,7 +44,7 @@ declare namespace MuBot.RepoClass {
    * @property {number} repoId 仓库 ID
    * @property {string} ownerName 仓库拥有者名称
    * @property {number} ownerId 仓库拥有者 ID
-   * @property {Record<string, string>} labels 仓库标签
+   * @property {Record<LabelKey, string>} labels 仓库标签
    * @return Config
    */
   interface Config {
@@ -28,7 +52,9 @@ declare namespace MuBot.RepoClass {
     repoId: number;
     ownerName: string;
     ownerId: number;
-    labels: Record<string, string>;
+    labels: {
+      [key in LabelKey]: string;
+    };
   }
 
   /**
