8ec87babf96fc7cea7488b40e4f7486cf0dc9819
BTMuli <BT-Muli@outlook.com>
2023-10-31 16:49:45 +0800

    🐛 card 用 tg 的 release handler

--- src/repo/genshin-card.ts
+++ src/repo/genshin-card.ts
@@ -7,6 +7,7 @@
 import type { Context } from "probot";
 
 import { BaseRepo } from "./base.ts";
+import handler from "../handler/teyvat-guide/handler.ts";
 
 /**
  * @description 原神卡片仓库类
@@ -27,28 +28,6 @@ class GenshinCardRepo extends BaseRepo {
   }
 
   /**
-   * @func issuesClosed
-   * @description issue closed 事件
-   * @since 1.0.0
-   * @param {Context<"issues.closed">} context probot context
-   * @returns {Promise<void>} void
-   */
-  protected async issuesClosed(context: Context<"issues.closed">): Promise<void> {
-    // await handler.issues.closed(context);
-  }
-
-  /**
-   * @func issueCommentCreated
-   * @description issue comment created 事件
-   * @since 1.0.0
-   * @param {Context<"issue_comment.created">} context probot context
-   * @returns {Promise<void>} void
-   */
-  protected async issueCommentCreated(context: Context<"issue_comment.created">): Promise<void> {
-    // await handler.issueComment.created(context, this);
-  }
-
-  /**
    * @func releasePublished
    * @description release published 事件
    * @since 1.0.0
@@ -56,7 +35,7 @@ class GenshinCardRepo extends BaseRepo {
    * @returns {Promise<void>} void
    */
   protected async releasePublished(context: Context<"release.published">): Promise<void> {
-    // await handler.release.published(context);
+    await handler.release.published(context);
   }
 }
 
